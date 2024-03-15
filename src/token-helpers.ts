cfb8b1afceed4c3b0169f539fd2432d288537403
PBillingsby <philbillingsby@gmail.com>
2024-03-15 14:20:44 -0400

    add starting code

--- src/token-helpers.ts
+++ src/token-helpers.ts
@@ -20,43 +20,5 @@ export async function createTokenWithMemoExtension(
   tokenAccountKeypair: Keypair,
   mint: PublicKey,
 ) {
-
-  const accountLen = getAccountLen([ExtensionType.MemoTransfer]);
-  // Minimum lamports required for Token Account
-  const lamports = await connection.getMinimumBalanceForRentExemption(accountLen);
-
-  const createAccountInstruction = SystemProgram.createAccount({
-    fromPubkey: payer.publicKey, // Account that will transfer lamports to created account
-    newAccountPubkey: tokenAccountKeypair.publicKey, // Address of the account to create
-    space: accountLen, // Amount of bytes to allocate to the created account
-    lamports, // Amount of lamports transferred to created account
-    programId: TOKEN_2022_PROGRAM_ID, // Program assigned as owner of created account
-  });
-
-  const initializeAccountInstruction = createInitializeAccountInstruction(
-    tokenAccountKeypair.publicKey, // Token Account Address
-    mint, // Mint Account
-    payer.publicKey, // Token Account Owner
-    TOKEN_2022_PROGRAM_ID, // Token Extension Program ID
-  );
-
-  const enableRequiredMemoTransfersInstruction =
-    createEnableRequiredMemoTransfersInstruction(
-      tokenAccountKeypair.publicKey, // Token Account address
-      payer.publicKey, // Token Account Owner
-      undefined, // Additional signers
-      TOKEN_2022_PROGRAM_ID, // Token Program ID
-    );
-
-  const transaction = new Transaction().add(
-    createAccountInstruction,
-    initializeAccountInstruction,
-    enableRequiredMemoTransfersInstruction,
-  );
-
-  const transactionSignature = await sendAndConfirmTransaction(
-    connection,
-    transaction,
-    [payer, tokenAccountKeypair], // Signers
-  );
+  throw new Error
 }
