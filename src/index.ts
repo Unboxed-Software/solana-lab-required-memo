5e2a06238b82125157c3a8dd92807c1e0ae70a06
PBillingsby <philbillingsby@gmail.com>
2024-03-21 13:33:11 -0400

    add refactored starting code with new helpers

--- src/index.ts
+++ src/index.ts
@@ -14,21 +14,23 @@ import {
   Transaction,
   PublicKey,
   TransactionInstruction,
+  LAMPORTS_PER_SOL,
 } from "@solana/web3.js";
-import { createTokenWithMemoExtension } from "./token-helpers";
-import { initializeKeypair } from "./keypair-helpers";
+import { createTokenWithMemoExtension } from "./token-helper";
+import { initializeKeypair, makeKeypairs } from '@solana-developers/helpers';
+
 require("dotenv").config();
 
 
 (async () => {
   const connection = new Connection("http://127.0.0.1:8899", 'confirmed');
-  const payer = await initializeKeypair(connection);
+  const payer = await initializeKeypair(connection, {
+    airdropAmount: 1 * LAMPORTS_PER_SOL
+  });
   const mintDecimals = 9;
 
-  const ourTokenAccountKeypair = Keypair.generate();
+  const [ourTokenAccountKeypair, otherTokenAccountKeypair] = makeKeypairs(2)
   const ourTokenAccount = ourTokenAccountKeypair.publicKey;
-
-  const otherTokenAccountKeypair = Keypair.generate();
   const otherTokenAccount = otherTokenAccountKeypair.publicKey;
 
   const amountToMint = 1000;
